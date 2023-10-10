d96034e31ee05bdb9a998e7c5dd2200b2a52faae
Tsuyoshi HARA <chloe463+git@gmail.com>
2023-10-11 01:38:05 +0900

    feat(enquete_service): Use pb-serializer

---
+++ app/serializers/enquete_serializer.rb
@@ -0,0 +1,32 @@
+class EnqueteSerializer < Pb::Serializer::Base
+  message Mypackage::Enquete::Enquete
+
+  attribute :id
+  attribute :title
+  attribute :description
+  attribute :status
+  attribute :start_at
+  attribute :end_at
+  attribute :created_at
+  attribute :updated_at
+
+  def status
+    "STATUS_#{object.status.upcase}".to_sym
+  end
+
+  def start_at
+    Pb.to_timestamp(object.start_at)
+  end
+
+  def end_at
+    Pb.to_timestamp(object.end_at)
+  end
+
+  def created_at
+    Pb.to_timestamp(object.created_at)
+  end
+
+  def updated_at
+    Pb.to_timestamp(object.updated_at)
+  end
+end
--- app/services/enquete_service.rb
+++ app/services/enquete_service.rb
@@ -7,32 +7,13 @@ class EnqueteService < Mypackage::Enquete::EnqueteService::Service
       .offset(req.offset)
       .limit(req.limit)
     enquetes = raw_enquetes.map do |e|
-      Mypackage::Enquete::Enquete.new(
-        id: e.id,
-        title: e.title,
-        description: e.description,
-        status: "STATUS_#{e.status.to_s.upcase}",
-        start_at: Google::Protobuf::Timestamp.new(seconds: e.start_at.to_i),
-        end_at: Google::Protobuf::Timestamp.new(seconds: e.end_at.to_i),
-        created_at: Google::Protobuf::Timestamp.new(seconds: e.created_at.to_i),
-        updated_at: Google::Protobuf::Timestamp.new(seconds: e.updated_at.to_i),
-      )
+      EnqueteSerializer.new(e).to_pb
     end
-    Mypackage::Enquete::ListEnqueteResponse.new(enquetes: enquetes.to_a)
+    Mypackage::Enquete::ListEnqueteResponse.new(enquetes: enquetes)
   end
 
   def get_enquete(req, _call)
     raw_enquete = Enquete.find(req.enquete_id)
-    enquete = Mypackage::Enquete::Enquete.new(
-      id: raw_enquete.id,
-      title: raw_enquete.title,
-      description: raw_enquete.description,
-      status: "STATUS_#{raw_enquete.status.to_s.upcase}",
-      start_at: Google::Protobuf::Timestamp.new(seconds: raw_enquete.start_at.to_i),
-      end_at: Google::Protobuf::Timestamp.new(seconds: raw_enquete.end_at.to_i),
-      created_at: Google::Protobuf::Timestamp.new(seconds: raw_enquete.created_at.to_i),
-      updated_at: Google::Protobuf::Timestamp.new(seconds: raw_enquete.updated_at.to_i),
-    )
-    Mypackage::Enquete::GetEnqueteResponse.new(enquete: enquete)
+    Mypackage::Enquete::GetEnqueteResponse.new(enquete: EnqueteSerializer.new(raw_enquete).to_pb)
   end
 end
