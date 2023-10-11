283b420626b3ce2763296027f5b6501049df37e8
Tsuyoshi HARA <chloe463+git@gmail.com>
2023-10-12 00:09:15 +0900

    style(enquete_service): Apply prettier

--- app/services/enquete_service.rb
+++ app/services/enquete_service.rb
@@ -1,15 +1,15 @@
 require 'google/protobuf/timestamp_pb'
-require_relative '../lib/enquete_services_pb.rb'
+require_relative '../lib/enquete_services_pb'
 
 class EnqueteService < Mypackage::Enquete::EnqueteService::Service
   def list_enquete(req, _unused_call)
     raw_enquetes = Enquete
-      .offset(req.offset)
-      .limit(req.limit)
+                   .offset(req.offset)
+                   .limit(req.limit)
     enquetes = raw_enquetes.map do |e|
       EnqueteSerializer.new(e).to_pb
     end
-    Mypackage::Enquete::ListEnqueteResponse.new(enquetes: enquetes)
+    Mypackage::Enquete::ListEnqueteResponse.new(enquetes:)
   end
 
   def get_enquete(req, _call)
